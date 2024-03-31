d1410fe8e4f91b230d3a0682e0c26c1836453092
Stephen <sun.stephen.oo@gmail.com>
2024-03-31 15:16:13 -0400

    style the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App () {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Eagleton Labs Logistics</h2>
+      <h2 style={{ background: 'gold' }}>Eagleton Labs Logistics</h2>
       <div className='card'>
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
