ff074c24e5a1fe5e3c735f0bf47ca3440c46c26f
Stephen <sun.stephen.oo@gmail.com>
2024-03-31 22:31:01 -0400

    adjust styling for mobile first

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App () {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Tailwind</h1>
       <h2 style={{ background: 'gold' }}>Eagleton Labs Logistics</h2>
       <ResponsiveText />
       <div className='card'>
